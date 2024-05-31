4f226f8e70f06885eba1e3282b837bf5ce6d15b3
FinnianX <85058938+FinnianX@users.noreply.github.com>
2024-05-31 11:04:24 +0200

    Add files via upload

--- allwordsinword.py
+++ allwordsinword.py
@@ -1,4 +1,3 @@
-#These are some functions for word processing
 # Function to check if canidate word is in target word. returns true/false
 def wordinword(canidate, target):
     if len(canidate) > len(target):
@@ -24,3 +23,13 @@ def return_all_words(allwords, word)
                     poswords.append(all)
                     possiblewords+=1
                 return poswords
+# this function takes a string as the argument and returns a dictionary where the keys is the word and the value is the amount of times it appears in the given argument
+def countwords(document):
+    sentences = document 
+    sentences = re.sub("[^A-Za-z -]","",sentences)
+    allwords = []
+    for token in nlp(sentences):
+        if token.pos_ not in excluded_tags:
+            allwords.append(token.text.lower())
+    amount_dict = {i:allwords.count(i) for i in allwords}
+    return amount_dict
